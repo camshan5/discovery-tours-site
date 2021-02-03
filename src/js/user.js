c97b62f0d01ee9a8a90911f8daa806c3bd10c669
Cameron Shannon <cameroncshannon@gmail.com>
2021-02-02 23:44:29 -0800

    Updates to payments page

--- src/js/user.js
+++ src/js/user.js
@@ -10,4 +10,42 @@ const yearsInBusiness = (yearFounded) => {
   return Math.abs(currentYear - yearFounded)
 }
 
-document.getElementById("years-in-business").innerHTML = yearsInBusiness(1983)
+const daysTo = () => {
+  let oneDay = 1000 * 60 * 60 * 24 // in milliseconds
+
+  let today = new Date()
+  let todayTime = today.getTime()
+  console.log(`todayTime = ${todayTime}`)
+
+  let currentYear = today.getFullYear()
+  // Month date math starts with Jan at 0
+  // getTime() converts to ms
+  let febDueDate = new Date(currentYear, 1, 15).getTime()
+  console.log(`febDueDate = ${febDueDate}`)
+
+  let marDueDate = new Date(currentYear, 2, 15).getTime()
+  let aprDueDate = new Date(currentYear, 3, 15).getTime()
+  let mayDueDate = new Date(currentYear, 4, 15).getTime()
+
+  if (febDueDate >= todayTime) {
+    let daysUntil = (febDueDate - todayTime) / oneDay
+    console.log(`daysUntil = ${Math.ceil(daysUntil)}`)
+
+    return Math.ceil(daysUntil)
+  } else if (marDueDate <= todayTime >= febDueDate) {
+    let daysUntil = (marDueDate - todayTime) / oneDay
+    return Math.ceil(daysUntil)
+  } else if (aprDueDate <= todayTime >= marDueDate) {
+    let daysUntil = (aprDueDate - todayTime) / oneDay
+    return Math.ceil(daysUntil)
+  } else if (mayDueDate <= todayTime >= aprDueDate) {
+    let daysUntil = (mayDueDate - todayTime) / oneDay
+    return Math.ceil(daysUntil)
+  } else if (todayTime > mayDueDate) {
+    return "Final payments due."
+  }
+}
+
+// document.querySelector(".days-to").innerHTML = daysTo()
+
+document.querySelector("#years-in-business").innerHTML = yearsInBusiness(1983)
